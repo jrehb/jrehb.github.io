eccc27314041a319ee21d3fa6a26f72f1ec22911
Juilus Rehbein <julius.rehbein@tum.de>
2026-03-11 15:51:21 +0100

    OGV tmp push

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+// Drop Down Menu
 const btn = document.getElementById("hamburger");
 const menu = document.getElementById("nav-menu");
+
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
   menu.classList.toggle("open");
 });
+
 // close menu when a link is clicked
 menu.querySelectorAll("a").forEach((a) => {
   a.addEventListener("click", () => {
